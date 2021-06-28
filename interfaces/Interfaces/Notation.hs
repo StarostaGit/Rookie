296697a9a9d0543352369788d9a1097180098b54
Piotr Wojtczak <piotr.m.wojtczak@gmail.com>
2021-06-28 21:18:12 +0200

    Add FEN notation parsing
    This change introduces a function for FEN to BoardState conversion,
    repurposing that record type outside of the Engine package
    to give the client an easy way to query legal moves.

---
+++ interfaces/Interfaces/Notation.hs
@@ -0,0 +1,76 @@
+module Interfaces.Notation (
+    parseFEN
+) where
+
+import Data.String
+import Data.Char
+import Data.Array
+import Data.List.Split
+import Data.Bits
+import Control.Monad.State
+import qualified Data.Map as Map
+import Engine.BitBoard
+import Engine.BoardState
+
+type FEN = String
+
+parseFEN :: FEN -> BoardState
+parseFEN fen =
+    let parse :: FEN -> State BoardState ()
+        parse fen = do
+            let [position, side, castling, ep, plyClock, move] = words fen
+                ranks = concat $ reverse $ splitOn "/" position
+            foldl (\ acc p -> acc >>= flip parseSquare p) (return 0) ranks
+            modify (\ board -> board { castling = parseCastling castling })
+            modify (\ board -> board { enPassant = if ep == "-" then 0 
+                                                   else square $ (ord (head ep) - ord 'a') + 8 * (ord (ep !! 1) - ord '1') })
+    in
+        execState (parse fen) emptyBoard
+
+parseSquare :: Int -> Char -> State BoardState Int
+parseSquare sq val = do
+    board <- get
+    if not $ isPiece val then
+        return $ sq + digitToInt val
+    else do
+        let (color, piece) = decodePiece val
+            pieceArr = pieces board
+            pieceBb = pieceArr ! color ! piece
+            oldSide = pieceArr ! color
+            newSide = oldSide // [(piece, pieceBb .|. square sq)]
+            newPieces = pieceArr // [(color, newSide)]
+        modify $ setPieces newPieces
+        return $ sq + 1
+
+parseCastling :: String -> Int
+parseCastling rights = foldl (\ acc (r, bb) -> if r `elem` rights then acc `xor` bb else acc) 0 [
+        ('K', castlingRight White King),
+        ('Q', castlingRight White Queen),
+        ('k', castlingRight Black King),
+        ('q', castlingRight Black Queen)
+    ]
+
+-- Helpers
+
+pieceMap :: Map.Map Char (Color, Piece)
+pieceMap = Map.fromList [
+        ('P', (White, Pawn)),
+        ('K', (White, King)),
+        ('Q', (White, Queen)),
+        ('B', (White, Bishop)),
+        ('N', (White, Knight)),
+        ('R', (White, Rook)),
+        ('p', (Black, Pawn)),
+        ('k', (Black, King)),
+        ('q', (Black, Queen)),
+        ('b', (Black, Bishop)),
+        ('n', (Black, Knight)),
+        ('r', (Black, Rook))
+    ]
+
+isPiece :: Char -> Bool
+isPiece p = p `elem` Map.keys pieceMap
+
+decodePiece :: Char -> (Color, Piece)
+decodePiece p = case Map.lookup p pieceMap of
+    Just val -> val
