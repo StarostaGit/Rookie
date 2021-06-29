3454ce914d18ab016cee6815103aae2b74a638c0
Wiktor Czyż <czyz.wiktor@gmail.com>
2021-06-29 12:54:20 +0200

    client: Fix image loading and refactor
    Introduced a new function `loadPieceImage` that is responsible for
    loading individual pieces and refactored some code.

--- client/Client/Settings.hs
+++ client/Client/Settings.hs
@@ -1,6 +1,11 @@
 module Client.Settings where
 
+import System.FilePath
+
+import Common.Types
+
 import qualified Graphics.UI.Threepenny as UI
+import Graphics.UI.Threepenny.Core
 
 
 canvasSize :: Int
@@ -9,4 +14,18 @@ canvasSize = 800
 tileSize :: Double
 tileSize = fromIntegral canvasSize / 8
 
+selectedTileColor :: UI.FillStyle
 selectedTileColor = UI.solidColor $ UI.RGBA {UI.red = 14, UI.green = 114, UI.blue = 39, UI.alpha = 0.5}
+
+loadPieceImage :: (Color, Piece) -> UI String
+loadPieceImage (color, piece) =
+    let colorName = if color == White then "w" else "b"
+        pieceName = case piece of
+                        Rook   -> "R"
+                        Knight -> "N"
+                        Bishop -> "B"
+                        Queen  -> "Q"
+                        King   -> "K"
+                        Pawn   -> "P"
+    in
+        UI.loadFile "image/png" $ "client/static/staunty" </> (colorName ++ pieceName) <.> ".png"
