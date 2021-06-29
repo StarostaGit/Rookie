095fd3b21db7eed9bfec31664a20390ff9eeb891
Wiktor Czyż <czyz.wiktor@gmail.com>
2021-06-29 20:27:05 +0200

    client: New module `GameInfo` and refactor
    This change introduces new module `GameInfo`.
    It is responsible for maintaining data about
    current state of the game. It will be later
    used in generating FENs.

--- client/Client/Draw.hs
+++ client/Client/Draw.hs
@@ -4,17 +4,15 @@ module Client.Draw (draw, drawGrid, handleClick, Color(..), Piece(..), Position)
 import Control.Monad
 import qualified Data.Map as Map
 import Data.IORef
+import Data.Maybe
 
 import Common.Types
 import Client.Settings
+import Client.GameInfo
 
 import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core
 
-
-type Position = (Int, Int)
-
-type MapRefs = (IORef (Map.Map Position (Color, Piece, Int)), IORef (Map.Map (Color, Piece) [Maybe Position]))
 
 drawGrid :: UI.Canvas -> UI ()
 drawGrid canvas = do
@@ -27,27 +25,24 @@ drawGrid canvas = do
         )
 
 
-draw :: [((Color, Piece), Element)] -> IORef (Maybe (Color, Piece, Int)) ->
-        IORef (Map.Map (Color, Piece) [Maybe Position]) -> UI.Canvas -> UI ()
-draw images pieceRef piecePositionsMapRef canvas = do
+draw :: [((Color, Piece), Element)] -> IORef GameInfo -> UI.Canvas -> UI ()
+draw images gameInfoRef canvas = do
     canvas # UI.clearCanvas
     canvas # drawGrid
 
-    piecePositionsMap <- liftIO $ readIORef piecePositionsMapRef
+    gameInfo <- liftIO $ readIORef gameInfoRef
 
-    currentPiece <- liftIO $ readIORef pieceRef
-
+    canvas # set' UI.fillStyle selectedTileColor
     runMaybeOperation $ do
-        (color, piece, num) <- currentPiece
-        positions <- Map.lookup (color, piece) piecePositionsMap
+        (color, piece, num) <- currentPiece gameInfo
+        positions <- Map.lookup (color, piece) $ piecePositionsMap gameInfo
         (posX, posY) <- positions !! num
         return $ do
-            canvas # set' UI.fillStyle selectedTileColor
             canvas # UI.fillRect (fromIntegral posX * 100, fromIntegral posY * 100)
                                  tileSize tileSize
 
     forM_ images (\((color, piece), img) -> do
-        let maybePositions = Map.lookup (color, piece) piecePositionsMap
+        let maybePositions = Map.lookup (color, piece) $ piecePositionsMap gameInfo
         case maybePositions of
             Nothing ->
                 return ()
@@ -58,128 +53,130 @@ draw images pieceRef piecePositionsMapRef canvas = do
                                         canvas # UI.drawImage img (fromIntegral i * 100, fromIntegral j * 100))
         )
 
-finalizeTurn :: IORef (Maybe (Color, Piece, Int)) -> IORef Color -> IO ()
-finalizeTurn currentPieceRef currentTurnRef = do
-    writeIORef currentPieceRef Nothing
-    nextTurn currentTurnRef
-
 canPromote :: Piece -> Int -> Color -> Bool
 canPromote piece posY color =
     piece == Pawn && ((posY == 0 && color == White)  || (posY == 7 && color == Black))
 
-enPassant :: Piece -> Position -> Position -> Bool
-enPassant piece (oldX, oldY) (newX, newY) =
+isEnPassant :: Piece -> Position -> Position -> Bool
+isEnPassant piece (oldX, oldY) (newX, newY) =
     (piece == Pawn) && (oldX /= newX)
 
 enPassantDeletePosition :: Position -> Position -> Position
 enPassantDeletePosition (oldX, oldY) (newX, newY) =
     (newX, oldY)
 
-movePiece :: Maybe Position -> Position -> IORef (Maybe (Color, Piece, Int)) -> IORef Color -> MapRefs -> IO ()
-movePiece currentPiecePositionMaybe clickedPosition currentPieceRef currentTurnRef
-          (positionPieceMapRef, piecePositionsMapRef) = do
-    positionPieceMap  <- readIORef positionPieceMapRef
-    piecePositionsMap <- readIORef piecePositionsMapRef
-    currentPiece      <- readIORef currentPieceRef
-    currentTurn       <- readIORef currentTurnRef
+movePiece :: Maybe Position -> Position -> IORef GameInfo -> IO ()
+movePiece currentPiecePositionMaybe clickedPosition gameInfoRef = do
+    gameInfo <- readIORef gameInfoRef
 
     runMaybeOperation $ do
         currentPiecePosition <- currentPiecePositionMaybe
-        (color, piece, num)  <- currentPiece
+        (color, piece, num)  <- currentPiece gameInfo
         let (_, clickedY) = clickedPosition
         return $ if canPromote piece clickedY color then do
-                let maybePositions = Map.lookup (color, Queen) piecePositionsMap
+                let maybePositions = Map.lookup (color, Queen) $ piecePositionsMap gameInfo
                     newNum = maybe 0 length maybePositions
-                writeIORef positionPieceMapRef  $ Map.insert clickedPosition (color, Queen, newNum)
-                                                $ Map.delete currentPiecePosition positionPieceMap
-                writeIORef piecePositionsMapRef $ Map.adjust (\xs -> xs ++ [Just clickedPosition]) (color, Queen)
-                                                $ Map.adjust (replaceNth num Nothing) (color, piece) piecePositionsMap
-                finalizeTurn currentPieceRef currentTurnRef
-            else if enPassant piece currentPiecePosition clickedPosition then do
+                    newBoard = Map.insert clickedPosition (color, Queen, newNum) $
+                               Map.delete currentPiecePosition $ board gameInfo
+                    newPiecePositionsMap = Map.adjust (\xs -> xs ++ [Just clickedPosition]) (color, Queen) $
+                                            Map.adjust (replaceNth num Nothing) (color, piece) $ piecePositionsMap gameInfo
+                writeIORef gameInfoRef $ gameInfo {
+                                                    board = newBoard,
+                                                    piecePositionsMap = newPiecePositionsMap,
+                                                    currentPiece = Nothing,
+                                                    turn = opposite color
+                                                  }
+            else if isEnPassant piece currentPiecePosition clickedPosition then do
                     let deletePos = enPassantDeletePosition currentPiecePosition clickedPosition
-                        deletePiece = Map.lookup deletePos positionPieceMap
-                    writeIORef positionPieceMapRef  $ Map.insert clickedPosition (color, piece, num)
-                                                    $ Map.delete currentPiecePosition
-                                                    $ Map.delete deletePos positionPieceMap
-                    case deletePiece of
-                        Nothing ->
-                            writeIORef piecePositionsMapRef $ Map.adjust (replaceNth num $ Just clickedPosition)
-                                                                         (color, piece) piecePositionsMap
-                        Just (deleteColor, deletePiece, deleteNum) ->
-                            writeIORef piecePositionsMapRef $ Map.adjust (replaceNth num $ Just clickedPosition)
-                                                                         (color, piece)
-                                                            $ Map.adjust (replaceNth deleteNum Nothing)
-                                                                         (deleteColor, deletePiece) piecePositionsMap
-                    finalizeTurn currentPieceRef currentTurnRef
+                        deletePiece = Map.lookup deletePos $ board gameInfo
+                        newBoard = Map.insert clickedPosition (color, piece, num) $
+                                   Map.delete currentPiecePosition $
+                                   Map.delete deletePos $ board gameInfo
+                        newPiecePositionsMap = case deletePiece of
+                            Nothing ->
+                                Map.adjust (replaceNth num $ Just clickedPosition) (color, piece) $ piecePositionsMap gameInfo
+                            Just (deleteColor, deletePiece, deleteNum) ->
+                                Map.adjust (replaceNth num $ Just clickedPosition) (color, piece) $
+                                Map.adjust (replaceNth deleteNum Nothing) (deleteColor, deletePiece) $
+                                           piecePositionsMap gameInfo
+                    writeIORef gameInfoRef $ gameInfo {
+                                                        board = newBoard,
+                                                        piecePositionsMap = newPiecePositionsMap,
+                                                        currentPiece = Nothing,
+                                                        turn = opposite color
+                                                      }
                 else do
-                    writeIORef positionPieceMapRef  $ Map.insert clickedPosition (color, piece, num)
-                                                    $ Map.delete currentPiecePosition positionPieceMap
-                    writeIORef piecePositionsMapRef $ Map.adjust (replaceNth num $ Just clickedPosition)
-                                                                 (color, piece) piecePositionsMap
-                    finalizeTurn currentPieceRef currentTurnRef
+                    let newBoard = Map.insert clickedPosition (color, piece, num) $
+                                   Map.delete currentPiecePosition $ board gameInfo
+                        newPiecePositionsMap = Map.adjust (replaceNth num $ Just clickedPosition) (color, piece) $
+                                                          piecePositionsMap gameInfo
+                    writeIORef gameInfoRef $ gameInfo {
+                                                        board = newBoard,
+                                                        piecePositionsMap = newPiecePositionsMap,
+                                                        currentPiece = Nothing,
+                                                        turn = opposite color
+                                                      }
 
-takePiece :: Maybe Position -> Position -> (Color, Piece, Int) -> IORef (Maybe (Color, Piece, Int)) ->
-             IORef Color -> MapRefs -> IO ()
-takePiece currentPiecePositionMaybe clickedPosition (clickedColor, clickedPiece, clickedNum) currentPieceRef
-          currentTurnRef (positionPieceMapRef, piecePositionsMapRef) = do
-    positionPieceMap  <- readIORef positionPieceMapRef
-    piecePositionsMap <- readIORef piecePositionsMapRef
-    currentPiece      <- readIORef currentPieceRef
-    currentTurn       <- readIORef currentTurnRef
+takePiece :: Maybe Position -> Position -> (Color, Piece, Int) -> IORef GameInfo -> IO ()
+takePiece currentPiecePositionMaybe clickedPosition (clickedColor, clickedPiece, clickedNum) gameInfoRef = do
+    gameInfo <- readIORef gameInfoRef
 
     runMaybeOperation $ do
         currentPiecePosition <- currentPiecePositionMaybe
-        (color, piece, num)  <- currentPiece
+        (color, piece, num)  <- currentPiece gameInfo
         let (_, clickedY) = clickedPosition
         return $ if canPromote piece clickedY color then do
-                let maybePositions = Map.lookup (color, Queen) piecePositionsMap
-                    newNum = maybe 0 length maybePositions
-                writeIORef positionPieceMapRef  $ Map.insert clickedPosition (color, Queen, newNum)
-                                                $ Map.delete currentPiecePosition positionPieceMap
-                writeIORef piecePositionsMapRef $ Map.adjust (\xs -> xs ++ [Just clickedPosition]) (color, Queen)
-                                                $ Map.adjust (replaceNth num Nothing) (color, piece)
-                                                $ Map.adjust (replaceNth clickedNum Nothing)
-                                                             (clickedColor, clickedPiece) piecePositionsMap
-                finalizeTurn currentPieceRef currentTurnRef else do
-                writeIORef positionPieceMapRef  $ Map.insert clickedPosition (color, piece, num)
-                                                $ Map.delete currentPiecePosition positionPieceMap
-                writeIORef piecePositionsMapRef $ Map.adjust (replaceNth num $ Just clickedPosition) (color, piece)
-                                                $ Map.adjust (replaceNth clickedNum Nothing)
-                                                             (clickedColor, clickedPiece) piecePositionsMap
-                finalizeTurn currentPieceRef currentTurnRef
+            let maybePositions = Map.lookup (color, Queen) $ piecePositionsMap gameInfo
+                newNum = maybe 0 length maybePositions
+                newBoard = Map.insert clickedPosition (color, Queen, newNum) $
+                           Map.delete currentPiecePosition $ board gameInfo
+                newPiecePositionsMap = Map.adjust (\xs -> xs ++ [Just clickedPosition]) (color, Queen) $
+                                       Map.adjust (replaceNth num Nothing) (color, piece) $
+                                       Map.adjust (replaceNth clickedNum Nothing) (clickedColor, clickedPiece) $
+                                                  piecePositionsMap gameInfo
+            writeIORef gameInfoRef $ gameInfo {
+                board = newBoard,
+                piecePositionsMap = newPiecePositionsMap,
+                currentPiece = Nothing,
+                turn = opposite color
+            }
+        else do
+            let newBoard = Map.insert clickedPosition (color, piece, num) $
+                           Map.delete currentPiecePosition $ board gameInfo
+                newPiecePositionsMap = Map.adjust (replaceNth num $ Just clickedPosition) (color, piece) $
+                                       Map.adjust (replaceNth clickedNum Nothing) (clickedColor, clickedPiece) $
+                                                  piecePositionsMap gameInfo
+            writeIORef gameInfoRef $ gameInfo {
+                board = newBoard,
+                piecePositionsMap = newPiecePositionsMap,
+                currentPiece = Nothing,
+                turn = opposite color
+            }
 
-handleClick :: (Double, Double) -> IORef (Maybe (Color, Piece, Int)) -> IORef Color -> MapRefs -> UI.Canvas -> UI ()
-handleClick (x, y) currentPieceRef currentTurnRef (positionPieceMapRef, piecePositionsMapRef) canvas = do
+handleClick :: (Double, Double) -> IORef GameInfo -> UI.Canvas -> UI ()
+handleClick (x, y) gameInfoRef canvas = do
     let clickedPosition = (floor (x / 100), floor (y / 100))
-    positionPieceMap  <- liftIO $ readIORef positionPieceMapRef
-    piecePositionsMap <- liftIO $ readIORef piecePositionsMapRef
-    currentPiece      <- liftIO $ readIORef currentPieceRef
-    currentTurn       <- liftIO $ readIORef currentTurnRef
+    gameInfo <- liftIO $ readIORef gameInfoRef
 
-    let clickedPiece = Map.lookup clickedPosition positionPieceMap
-        currentPiecePosition = do (color, piece, num) <- currentPiece
-                                  result <- Map.lookup (color, piece) piecePositionsMap
+    let clickedPiece = Map.lookup clickedPosition $ board gameInfo
+        currentPiecePosition = do (color, piece, num) <- currentPiece gameInfo
+                                  result <- Map.lookup (color, piece) $ piecePositionsMap gameInfo
                                   result !! num
 
     liftIO $ case clickedPiece of
         Nothing ->
-            movePiece currentPiecePosition clickedPosition currentPieceRef currentTurnRef
-                      (positionPieceMapRef, piecePositionsMapRef)
-        Just (clickedColor, clickedPiece, clickedNum) | clickedColor /= currentTurn ->
-            takePiece currentPiecePosition clickedPosition (clickedColor, clickedPiece, clickedNum)
-                      currentPieceRef currentTurnRef (positionPieceMapRef, piecePositionsMapRef)
-        Just (color, piece, num) | color == currentTurn ->
-            writeIORef currentPieceRef $ Just (currentTurn, piece, num)
+            movePiece currentPiecePosition clickedPosition gameInfoRef
+        Just (clickedColor, clickedPiece, clickedNum) | clickedColor /= turn gameInfo ->
+            takePiece currentPiecePosition clickedPosition (clickedColor, clickedPiece, clickedNum) gameInfoRef
+        Just (color, piece, num) | color == turn gameInfo ->
+            writeIORef gameInfoRef $ gameInfo { currentPiece = Just (color, piece, num) }
         _ ->
             return ()
 
 -- Helpers
 
 runMaybeOperation :: Monad m => Maybe (m ()) -> m ()
-runMaybeOperation m =
-    case m of
-        Nothing        -> return ()
-        Just operation -> operation
+runMaybeOperation = fromMaybe (return ())
 
 replaceNth :: Int -> a -> [a] -> [a]
 replaceNth n newVal xs =
@@ -191,9 +188,3 @@ deleteNth n xs =
     let (left, _:right) = splitAt n xs
     in  left ++ right
 
-nextTurn :: IORef Color -> IO ()
-nextTurn currentTurnRef = do
-    val <- readIORef currentTurnRef
-    case val of
-        White -> writeIORef currentTurnRef Black
-        Black -> writeIORef currentTurnRef White
--- client/Client/Main.hs
+++ client/Client/Main.hs
@@ -2,13 +2,13 @@ import Control.Monad
 import qualified Data.Map as Map
 import Data.Tuple.Select
 import Data.IORef
-import Data.Bifunctor
 import Data.Array
 
 import Common.Types
 
 import Client.Draw
 import Client.Settings
+import Client.GameInfo
 
 import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core
@@ -51,55 +51,17 @@ setup window = void $ do
         let list = (info, img) : unpackedAcc
         (return list :: UI [((Color, Piece), Element)])) (return [] :: UI [((Color, Piece), Element)]) fileInfos
 
-    let positionPieceMap :: Map.Map Position (Color, Piece, Int)
-        positionPieceMap = Map.fromList $ [((0, 7), (White, Rook,   0)),
-                                           ((7, 7), (White, Rook,   1)),
-                                           ((0, 0), (Black, Rook,   0)),
-                                           ((7, 0), (Black, Rook,   1)),
-                                           ((1, 7), (White, Knight, 0)),
-                                           ((6, 7), (White, Knight, 1)),
-                                           ((1, 0), (Black, Knight, 0)),
-                                           ((6, 0), (Black, Knight, 1)),
-                                           ((2, 7), (White, Bishop, 0)),
-                                           ((5, 7), (White, Bishop, 1)),
-                                           ((2, 0), (Black, Bishop, 0)),
-                                           ((5, 0), (Black, Bishop, 1)),
-                                           ((3, 7), (White, Queen,  0)),
-                                           ((3, 0), (Black, Queen,  0)),
-                                           ((4, 7), (White, King,   0)),
-                                           ((4, 0), (Black, King,   0))] ++
-                                          [((i, 6), (White, Pawn,   i)) | i <- [0..7]] ++
-                                          [((i, 1), (Black, Pawn,   i)) | i <- [0..7]]
-
-    let piecePositionsMap :: Map.Map (Color, Piece) [Maybe Position]
-        piecePositionsMap = Map.fromList $ map (second (map Just))
-                                      [((White, Rook), [(0, 7), (7, 7)]),
-                                       ((Black, Rook),    [(0, 0), (7, 0)]),
-                                       ((White, Knight),  [(1, 7), (6, 7)]),
-                                       ((Black, Knight),  [(1, 0), (6, 0)]),
-                                       ((White, Bishop),  [(2, 7), (5, 7)]),
-                                       ((Black, Bishop),  [(2, 0), (5, 0)]),
-                                       ((White, Queen),   [(3, 7)]),
-                                       ((Black, Queen),   [(3, 0)]),
-                                       ((White, King),    [(4, 7)]),
-                                       ((Black, King),    [(4, 0)]),
-                                       ((White, Pawn),    [(i, 6) | i <- [0..7]]),
-                                       ((Black, Pawn),    [(i, 1) | i <- [0..7]])]
-
-    currentPieceRef      <- liftIO $ newIORef Nothing
-    currentTurnRef       <- liftIO $ newIORef White
-    positionPieceMapRef  <- liftIO $ newIORef positionPieceMap
-    piecePositionsMapRef <- liftIO $ newIORef piecePositionsMap
+    gameInfoRef <- liftIO $ newIORef initGameInfo
 
     canvas # drawGrid
 
     on UI.click startGame $ const $ do
-        canvas # draw images currentPieceRef piecePositionsMapRef
+        canvas # draw images gameInfoRef
 
     on UI.click clear $ const $ do
         canvas # UI.clearCanvas
 
     on UI.mousedown canvas $ \xy -> do
         canvas # UI.clearCanvas
-        canvas # handleClick xy currentPieceRef currentTurnRef (positionPieceMapRef, piecePositionsMapRef)
-        canvas # draw images currentPieceRef piecePositionsMapRef
+        canvas # handleClick xy gameInfoRef
+        canvas # draw images gameInfoRef
