6537f8fff252961bba2cbf7e9c00c93e3f50998b
Piotr Wojtczak <piotr.m.wojtczak@gmail.com>
2021-06-30 23:42:35 +0200

    engine: Fix reversed side to move in minmax search

--- engine/Engine/Search.hs
+++ engine/Engine/Search.hs
@@ -25,14 +25,15 @@ search :: BoardState -> Maybe Move
 search board =
     let side = sideToMove board
         moves = genMoves board
-        evaluated = map (\ move -> (minmax (makeMove move board) side maxDepth, move)) moves
+        evaluated = map (\ move -> (minmax (makeMove move board) (maxDepth - 1), move)) moves
     in
         if null evaluated then
             Nothing 
         else
             Just $ snd $ foldl1 (best side) evaluated
 
-minmax :: BoardState -> Color -> Int -> Float
-minmax board player 0 = evaluate board
-minmax board player depth = foldl (best player) (horizon player) moves
-    where moves = map (\ move -> minmax (makeMove move board) (opposite player) (depth - 1)) $ genMoves board
+minmax :: BoardState -> Int -> Float
+minmax board 0 = evaluate board
+minmax board depth = foldl (best player) (horizon player) moves
+    where player = sideToMove board
+          moves = map (\ move -> minmax (makeMove move board) (depth - 1)) $ genMoves board
