d581e2b06a523093a2b70c11f4c5a9e06ba3d685
Piotr Wojtczak <piotr.m.wojtczak@gmail.com>
2021-06-30 07:56:47 +0200

    engine: Add minmax searching algorithm to the search function
    Enhance the search slightly by including a barebone minmax game tree
    search

--- engine/Engine/Search.hs
+++ engine/Engine/Search.hs
@@ -8,19 +8,31 @@ import Engine.Moves
 import Engine.Eval
 
 -- Define which way the player optimizes
-best :: Color -> ((Float, Move) -> (Float, Move) -> (Float, Move))
+best :: Ord a => Color -> a -> a -> a
 best White = max
 best Black = min
 
+horizon :: Color -> Float
+horizon White = fromIntegral $ fst $ floatRange 0.0
+horizon Black = fromIntegral $ snd $ floatRange 0.0
+
 -- Searching algorithm returning best move
+
+maxDepth :: Int
+maxDepth = 2
 
 search :: BoardState -> Maybe Move
 search board =
     let side = sideToMove board
         moves = genMoves board
-        evaluated = map (\ move -> (evaluate $ makeMove move board, move)) moves
+        evaluated = map (\ move -> (minmax (makeMove move board) side maxDepth, move)) moves
     in
         if null evaluated then
             Nothing 
         else
             Just $ snd $ foldl1 (best side) evaluated
+
+minmax :: BoardState -> Color -> Int -> Float
+minmax board player 0 = evaluate board
+minmax board player depth = foldl (best player) (horizon player) moves
+    where moves = map (\ move -> minmax (makeMove move board) (opposite player) (depth - 1)) $ genMoves board
