7c7a8939ea7d39d7e3aeca395dbe40ed73540081
Piotr Wojtczak <piotr.m.wojtczak@gmail.com>
2021-07-01 11:43:46 +0200

    interfaces: Don't send moves exposing the king to the client
    For the purposes of the engine we only generate pseudo legal
    moves as there is no need for the explicit check - the engine
    will never want to lose the king.
    However, in the exposed interface for the legal moves we should
    filter out the moves that leave the king in check.

--- interfaces/Interfaces/LegalMoves.hs
+++ interfaces/Interfaces/LegalMoves.hs
@@ -5,9 +5,9 @@ module Interfaces.LegalMoves (
 import Common.Types
 import Engine.BoardState
 import qualified Engine.BitBoard as BitBoard
-import qualified Engine.Moves (getValidMoves)
+import qualified Engine.Moves (genMoves, isLegal, Move (..))
 
 getLegalMoves :: BoardState -> Square -> [Square]
 getLegalMoves board sq = case getPieceAt board sq of
     Nothing         -> []
-    Just (color, p) -> BitBoard.toSquares $ Engine.Moves.getValidMoves board color p (BitBoard.square $ fromEnum sq)
+    Just (color, p) -> map Engine.Moves.to $ filter ((== sq) . Engine.Moves.from) $ filter (`Engine.Moves.isLegal` board) $ Engine.Moves.genMoves board
