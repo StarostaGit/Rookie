3fe66686e8141f37d401437713aa2106d94d75ef
Wiktor Czyż <czyz.wiktor@gmail.com>
2021-07-16 23:22:12 +0200

    client & build: Basic configuration for shpadoinkle
    Simple web page with canvas using Shpadoinkle library is also provided.

--- client/Client/Main.hs
+++ client/Client/Main.hs
@@ -1,73 +1,28 @@
-import Control.Monad
-import qualified Data.Map as Map
-import Data.Tuple.Select
-import Data.IORef
-import Data.Array
+{-# LANGUAGE OverloadedStrings #-}
 
-import Common.Types
 
-import Client.Draw
-import Client.Settings
-import Client.GameInfo
+module Main where
 
-import qualified Graphics.UI.Threepenny as UI
-import Graphics.UI.Threepenny.Core
 
-import qualified Data.ByteString.Char8 as C
-import Network.Socket
-import Network.Socket.ByteString (recv, sendAll)
-import Network.Run.TCP
+import           Shpadoinkle                 (Html, JSM, h, textProp)
+import           Shpadoinkle.Backend.ParDiff (runParDiff)
+import           Shpadoinkle.Html
+import           Shpadoinkle.Run             (live, runJSorWarp, simple)
+
+
+view :: () -> Html m ()
+view _ = viewCanvas
+-- view _ = h "div" [] [ text "bar" ]
+
+viewCanvas :: Html m ()
+viewCanvas = canvas [id' "myCanvas", ("width",  textProp "800"), ("height", textProp "800"), ("style", textProp "border:1px solid #000000;")] []
+
+app :: JSM ()
+app = simple runParDiff () view getBody
 
 
 main :: IO ()
 main = do
-    runTCPClient "127.0.0.1" "3000" $ \s -> do
-        startGUI defaultConfig $ setup s
-
-type PieceFile = ((Color, Piece), UI Element)
-
-setup :: Socket -> Window -> UI ()
-setup socket window = void $ do
-    return window # set title "Rookie"
-
-    -- initialize board
-    canvas <- UI.canvas
-        # set UI.height canvasSize
-        # set UI.width  canvasSize
-        # set style [("border", "solid black 1px"), ("background", "#eee")]
-
-    startGame <- UI.button #+ [string "Start game"]
-    delete    <- UI.button #+ [string "Delete"]
-    clear     <- UI.button #+ [string "Clear"]
-
-    getBody window #+
-        [ column [element canvas]
-        , element startGame, element clear
-        ]
-
-    let fileInfos = [(\x -> (x, loadPieceImage x)) (color, piece) | color <- range (White, Black),
-                                                                    piece <- range (Pawn, King)]
-
-    images <- foldl (\acc fileInfo -> do
-        let info = sel1 fileInfo
-            file = sel2 fileInfo
-        url <- file
-        unpackedAcc <- acc
-        img <- UI.img # set UI.src url
-        let list = (info, img) : unpackedAcc
-        (return list :: UI [((Color, Piece), Element)])) (return [] :: UI [((Color, Piece), Element)]) fileInfos
-
-    gameInfoRef <- liftIO $ newIORef initGameInfo
-
-    canvas # drawGrid
-
-    on UI.click startGame $ const $ do
-        canvas # draw images gameInfoRef
-
-    on UI.click clear $ const $ do
-        canvas # UI.clearCanvas
-
-    on UI.mousedown canvas $ \xy -> do
-        canvas # handleClick xy gameInfoRef socket
-        canvas # UI.clearCanvas
-        canvas # draw images gameInfoRef
+    let port = 8080
+    putStrLn $ "Frontend running on https://localhost:" ++ show port ++ "\n"
+    runJSorWarp port app
